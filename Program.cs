765d2419958e34c8a94e4f9c77ca807dc6ae2ec0
abissier <71886314+abissier@users.noreply.github.com>
2021-06-14 16:07:27 -0700

    file commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit) ");
+                string firstName = Console.ReadLine();
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Please enter a last name: ");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Please enter  your ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a photoUrl");
+                string photoUrl = Console.ReadLine();
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+        static void Main(string[] args)
+
+        {
+            List<Employee> employees = GetEmployees();
+
+            Util.PrintEmployees(employees);
+
+            Util.MakeCSV(employees);
+        }
+    }
+}
